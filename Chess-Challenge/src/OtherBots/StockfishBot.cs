98de7cb4f226d8dbe72f38adbf14fc7ec42d33d0
adahlkvist <AugustDahlkvist@gmail.com>
2023-07-24 20:03:45 +0100

    Add some Stockfish files

---
+++ Chess-Challenge/src/OtherBots/StockfishBot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Stockfish;
+using ChessChallenge.API;
+using Stockfish.NET;
+
+public class StockfishBot : IChessBot
+{
+	const int STOCKFISH_LEVEL = 3;
+
+	IStockfish mStockFish;
+
+	public StockfishBot()
+	{
+		
+	}
+	
+	public Move Think(Board board, Timer timer)
+	{
+
+	}
+}
